c03711e81b46b2d4a10c332e461c165ccc8ecae3
Qin Siping <365983009@qq.com>
2018-04-16 09:29:21 +0800

    EG02
    外部类的方法，返回一个指向内部类的引用

--- C10/Parcell.java
+++ C10/Parcell.java
@@ -16,13 +16,22 @@ public class Parcell {
 			return label;
 		}
 	}
+	public Destination to(String s) {
+		return new Destination(s);
+	}
+	public Contents contents() {
+		return new Contents();
+	}
 	public void ship(String destination) {
 		Contents con = new Contents();
 		Destination dest = new Destination(destination);
 		System.out.println(dest.readLabel());
 	}
 	public static void main(String[] args) {
-		Parcell par = new Parcell();
-		par.ship("China");
+		Parcell par1 = new Parcell();
+		par1.ship("China");
+		Parcell par2 = new Parcell();
+		Parcell.Contents con = par2.contents();
+		Parcell.Destination dest = par2.to("U.S");
 	}
 }
