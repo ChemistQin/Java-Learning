b74d224d2a4b684bb33db4f41ae08c08e4f5c30c
Qin Siping <365983009@qq.com>
2018-04-23 20:10:32 +0800

    从多层嵌套类中访问外部类成员

---
+++ C10/MultiNestingAccess.java
@@ -0,0 +1,24 @@
+package C10;
+
+class MNA {
+	private void f() {}
+	class A {
+		private void g() {}
+		public class B {
+			void h() {
+				g();
+				f();
+				System.out.println("h() : g(), f()");
+			}
+		}
+	}
+}
+
+public class MultiNestingAccess {
+	public static void main(String[] args) {
+		MNA mna = new MNA();
+		MNA.A mnaa = mna.new A();
+		MNA.A.B mnaab = mnaa.new B();
+		mnaab.h();
+	}
+}
