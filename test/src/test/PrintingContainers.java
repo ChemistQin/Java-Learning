3bde9e64f2457812ddde4fb7a46ea5c4b9052594
Qin Siping <365983009@qq.com>
2018-04-25 23:05:07 +0800

    container
    试用IDE

---
+++ test/src/test/PrintingContainers.java
@@ -0,0 +1,30 @@
+package test;
+import java.util.*;
+
+public class PrintingContainers {
+
+	static Collection fill(Collection<String> colle) {
+		colle.add("rat");
+		colle.add("cat");
+		colle.add("dog");
+		return colle;
+	}
+	static Map fill(Map<String,String> map) {
+		map.put("rat", "Fuzzy");
+		map.put("cat", "Rags");
+		map.put("dog", "Bosco");
+		return map;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(fill(new ArrayList<String> ()));
+		System.out.println(fill(new LinkedList<String> ()));
+		System.out.println(fill(new HashSet<String> ()));
+		System.out.println(fill(new TreeSet<String> ()));
+		System.out.println(fill(new LinkedHashSet<String> ()));
+		System.out.println(fill(new HashMap<String,String> ()));
+		System.out.println(fill(new TreeMap<String,String> ()));
+		System.out.println(fill(new LinkedHashMap<String,String> ()));
+	}
+
+}
--- test/src/test/SC.java
+++ test/src/test/SC.java
@@ -1,11 +1,14 @@
 package test;
 
+import java.util.*;
+
 public class SC {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println(6666666);
-
+		Collection<Integer> colle = new ArrayList<Integer>(); 
+		for(int i = 0; i < 5; i++)
+			colle.add(i);
+		for(Integer i : colle)
+			System.out.println(i);
 	}
-
 }
