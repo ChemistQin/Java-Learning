9ecb385064801e3ba8bfc7a7a7bd31bba79b8767
Qin Siping <365983009@qq.com>
2018-06-06 17:43:32 +0800

    quicksort

--- Algorithm/src/algorithm/Sorter.java
+++ Algorithm/src/algorithm/Sorter.java
@@ -1,13 +1,17 @@
 package algorithm;
 
+
 public class Sorter {
 	public void selectSort(int[] array) {
 		for (int i = 0; i < array.length; i++) {
 			for (int j = i + 1; j < array.length; j++) {
 				if (array[i] > array[j]) {
+					/*
 					array[i] ^= array[j];  
 					array[j] ^= array[i]; 
 					array[i] ^= array[j];
+					*/
+					swap(array, i, j);
 				}
 			}
 		}
@@ -25,5 +29,45 @@ public class Sorter {
 			}
 		}
 	}
+	
+	public void quickSort(int[] array, int begin, int end) {
+		if (begin >= end || array == null || array.length <= 1) {
+            return;
+        }
+        int i = begin, j = end, pivot = array[(begin + end) / 2];
+        while (i <= j) {
+            while (array[i] < pivot) {
+                ++i;
+            }
+            while (array[j] > pivot) {
+                --j;
+            }
+            if (i < j) {
+                swap(array, i, j);
+                ++i;
+                --j;
+            } else if (i == j) {
+                ++i;
+            }
+        }
+        quickSort(array, begin, j);
+        quickSort(array, i, end);
+	}
+	
+	public static void swap(int[] array, int i, int j) {
+		int t = array[i];
+		array[i] = array[j];
+		array[j] = t;
+	}
 
 }
+
+/*test
+ * int[] array = {3, 2, 5, 7, 13, 21, 19, 23, 11};
+		Sorter sorter = new Sorter();
+		sorter.insertSort(array);
+		for (int i : array) {
+			System.out.print(i + " ");
+		}
+ */
+
--- Algorithm/src/algorithm/TestDriver.java
+++ Algorithm/src/algorithm/TestDriver.java
@@ -4,7 +4,8 @@ public class TestDriver {
 	public static void main(String[] args) {
 		int[] array = {3, 2, 5, 7, 13, 21, 19, 23, 11};
 		Sorter sorter = new Sorter();
-		sorter.insertSort(array);
+		//sorter.selectSort(array);
+		sorter.quickSort(array, 0, array.length - 1);
 		for (int i : array) {
 			System.out.print(i + " ");
 		}
