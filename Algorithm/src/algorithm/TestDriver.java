ae54169fcb3b72cb1f8aee60a5e90fadc9d28def
Qin Siping <365983009@qq.com>
2018-06-07 00:01:01 +0800

    birthday
    Happy 24th Birthday
    Happy 24th Birthday
    Happy 24th Birthday

--- Algorithm/src/algorithm/TestDriver.java
+++ Algorithm/src/algorithm/TestDriver.java
@@ -2,6 +2,7 @@ package algorithm;
 
 public class TestDriver {
 	public static void main(String[] args) {
+		/*
 		int[] array = {3, 2, 5, 7, 13, 21, 19, 23, 11};
 		Sorter sorter = new Sorter();
 		//sorter.selectSort(array);
@@ -9,5 +10,10 @@ public class TestDriver {
 		for (int i : array) {
 			System.out.print(i + " ");
 		}
+		*/
+		
+		System.out.println("Happy " + 24 + "th Birthday");
+		System.out.println("Happy " + 0b00011000 + "th Birthday");
+		System.out.println("Happy " + 0x18 + "th Birthday");
 	}
 }
