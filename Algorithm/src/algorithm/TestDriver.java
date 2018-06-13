9f35ab7943023e2827e6f5c83d6933855155715f
Qin Siping <365983009@qq.com>
2018-06-13 22:19:01 +0800

    困了

--- Algorithm/src/algorithm/TestDriver.java
+++ Algorithm/src/algorithm/TestDriver.java
@@ -24,4 +24,17 @@ June 7, 2018
 System.out.println("Happy " + 0b00011000 + "th Birthday");
 System.out.println("Happy " + 24 + "th Birthday");
 System.out.println("Happy " + 0x18 + "th Birthday");
+
+	int[][] arr = new int[5][5];
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+[I@61064425
+[I@7b1d7fff
+[I@299a06ac
+[I@383534aa
+[I@6bc168e5
+
+二维数组...arr[i] 值为地址
+		
 */
