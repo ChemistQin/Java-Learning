2618d4b27f9f8e05da4363957c40b0a96b08484a
Qin Siping <365983009@qq.com>
2018-07-12 11:47:10 +0800

    some demo

---
+++ Algorithm/src/algorithm/MathTest.java
@@ -0,0 +1,11 @@
+package algorithm;
+
+public class MathTest {
+	public static void main(String[] args) {
+		System.out.println(Math.E);
+		System.out.println(Math.log(Math.E));
+		System.out.println(Math.floor(1.1));
+		System.out.println(Math.ceil(1.1));
+		System.out.println(Math.random());
+	}
+}
---
+++ Algorithm/src/algorithm/NumberTest.java
@@ -0,0 +1,24 @@
+package algorithm;
+
+public class NumberTest {
+	public static void main(String[] args) {
+		Integer x = 6;
+		System.out.println(x.intValue());
+		System.out.println(x.floatValue());
+		System.out.println(Integer.toHexString(1314));
+		System.out.println(Integer.toBinaryString(1111));
+		System.out.println(Integer.parseInt("kona", 27));
+		/*
+		 * 27进制
+		 * 0 1 2 3 4 5 6 7 8 9 
+		 * A B C D E F G H I J 
+		 * K L M N O P Q R S T 
+		 * U V W X Y Z
+		 * Kona = 21 * 27 ^ 3 + 25 * 27 ^ 2 + 24 * 27 + 10
+		 */
+		System.out.println(20 * Math.pow(27, 3) + 
+						   24 * Math.pow(27, 2) + 
+						   23 * Math.pow(27, 1) +
+						   10 * Math.pow(27, 0));
+	}
+}
