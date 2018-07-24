0667c47398695e124d14ea7737dbd4008a35698c
Qin Siping <365983009@qq.com>
2018-07-24 22:41:28 +0800

    IO流

--- Algorithm/src/langtest/StreamTest.java
+++ Algorithm/src/langtest/StreamTest.java
@@ -4,10 +4,48 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/**
+ * 
+ * @author Qinsiping
+ * I/O Stream ----> ByteStream ----> Reader ----> BufferedReader
+ * 											----> InputStreamReader ----> FileReader
+ * 											----> StringReader
+ * 											----> PipedReader
+ * 											----> CharArrayReader
+ * 											----> FilterReader ----> PushbackReader 
+ * 									 
+ * 									 Writer ----> BufferedWriter 
+ * 											----> OutputStreamWriter ----> FileWriter
+ * 											----> PrinterWriter			
+ * 											----> StringWriter	
+ * 											----> PipedWriter	
+ * 											----> CharArrayWriter	
+ * 											----> FilterWriter		
+ * 										
+ *            ----> CharStream ----> InputStream ----> FileInputStream
+ *            									 ----> FilterInputStream ----> BufferedInputStream
+ *            															 ----> DataInputStream
+ *            															 ----> PushbackInputStream
+ *             									 ----> ObjectInputStream
+ *            									 ----> PipedInputStream
+ *             									 ----> SequenceInputStream
+ *              								 ----> StringBufferInputStream
+ *               								 ----> ByteArrayInputStream
+ *           
+ *            				  ----> OutputStream ----> FileOutputStream  
+ *               								 ----> FilterOutputStream ----> BufferedOutputStream
+ *            															  ----> DataOutputStream
+ *            															  ----> PrintStream
+ *               								 ----> ObjectOutputStream
+ *               								 ----> PipedOutputStream
+ *               								 ----> ByteArrayOutputStream                                                                 
+ * 
+ */
+
 public class StreamTest {
 	public static void main(String[] args) throws IOException{
 		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-		System.out.println("Input:\n-----PRESS 'Q' TO QUIT-----");
+		System.out.println("-----PRESS 'Q' TO QUIT-----\nInput:");
 		char c;
 		do {
 			c = (char) bufferedReader.read();
