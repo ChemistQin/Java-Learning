7b772d554507a1384d1718cc73f64957b06b753d
Qin Siping <365983009@qq.com>
2018-08-09 22:42:56 +0800

    map

--- Algorithm/src/containers/CollectionMethod.java
+++ Algorithm/src/containers/CollectionMethod.java
@@ -2,10 +2,22 @@ package containers;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 
 public class CollectionMethod {
 	public static void main(String[] args) {
 		Collection<String> collection = new ArrayList<>();
-		//collection.addAll();
+		collection.addAll(Countries.names(6));
+		collection.add("ten");
+		collection.add("eleven");
+		System.out.println(collection);
+		
+		Object[] array = collection.toArray();
+		String[] strings = collection.toArray(new String[0]);
+		
+		System.out.println(Collections.max(collection));
+		System.out.println(Collections.min(collection));
+		
+		
 	}
 }
